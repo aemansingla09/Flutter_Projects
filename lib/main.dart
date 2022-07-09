482b9701cc1eca9fdb488c22f7aa03fce73e0b44
Abhay Garg <abhaygarg12>
2022-07-09 13:10:58 +0530

    adding test func.

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,17 @@ void main() {
   runApp(const MyApp());
 }
 
+test() {
+  print('this is abhay');
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    test();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
