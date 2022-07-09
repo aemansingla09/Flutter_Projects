ce69ce4bff0cacd863d18efb1b9abc904aad3758
Aeman <aemansingla09@gmail.com>
2022-07-09 13:52:05 +0530

    Home Page

---
+++ lib/pages/root.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RootApp extends StatefulWidget {
+  RootApp({Key? key}) : super(key: key);
+
+  @override
+  State<RootApp> createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
