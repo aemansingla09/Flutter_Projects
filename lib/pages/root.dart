c15e7cf2c85acd26853b569e4da4eee0da52c018
Abhay Garg <abhaygarg12>
2022-07-14 22:39:36 +0530

    abhay

--- lib/pages/root.dart
+++ lib/pages/root.dart
@@ -11,7 +11,7 @@ class _RootAppState extends State<RootApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.blue,
     );
   }
 }
